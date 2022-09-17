784535b4c67d3243dbad5a9e08b3f831d62e3954
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-09-17 12:58:10 +0700

    script untuk folder awal

---
+++ buat_folder_awal.sh
@@ -0,0 +1,24 @@
+# Meminta konfirmasi pembuatan folder
+echo "Apakah ingin membuat folder awal? [y/n]"
+read masukan
+
+if [[ $masukan == "y" ]]; then
+
+    mkdir tmp-submission
+
+    # Pembuatan masing-masing folder Lab
+    for i in {01..10}
+    do
+        mkdir -p solusi/Lab$i/input
+        mkdir -p solusi/Lab$i/output
+    done
+
+    for i in {01.04}
+    do
+        mkdir -p solusi/TP$i/input
+        mkdir -p solusi/TP$i/output
+    done
+    
+else
+    echo "Folder tidak dibuat"
+fi
